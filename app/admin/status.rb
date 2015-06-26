640c9e7ccf52d492c2b26a46e0f1cb04fa626969
Ricky Chilcott <ricky@rbsavvy.com>
2015-06-26 00:35:36 -0400

    Set last_out_update and reported_out_count to defaults before save

--- app/admin/status.rb
+++ app/admin/status.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Status do
   permit_params :establishment_id, :beer_id, :status
   
-  actions :index, :show
+  actions :all, except: [:destroy]
 
   index do
     id_column
@@ -11,14 +11,21 @@ ActiveAdmin.register Status do
   end
   
   form do |f|
-      inputs 'Details' do
-        input :establishment, :input_html => { :disabled => true }
-        input :beer, :input_html => { :disabled => true }
-        input :status, as: :radio, collection: STATUS_OPTIONS
-        input :last_out_update, as: :string, :input_html => { :disabled => true }
-        input :reported_out_count, :input_html => { :disabled => true }
-      end
-
-      actions
+    inputs 'Details' do
+      input :establishment
+      input :beer
+      input :status, as: :radio, collection: STATUS_OPTIONS
+      input :last_out_update, as: :string, :input_html => { :disabled => true }
+      input :reported_out_count, :input_html => { :disabled => true }
     end
+
+    actions
+  end
+    
+  controller do
+    before_save do |status|
+      status.last_out_update = Time.now
+      status.reported_out_count = 0
+    end
+  end
 end
