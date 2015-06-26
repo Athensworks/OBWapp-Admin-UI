cfb83183924148fded58a7c64060a69d7d276f37
Ricky Chilcott <ricky@rbsavvy.com>
2015-06-26 03:32:57 -0400

    Add a google map when rendering Establishment show

--- app/admin/establishment.rb
+++ app/admin/establishment.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register Establishment do
   permit_params :name, :lat, :lon, :address
-  
+
+  show do      
+    attributes_table do
+      row :name
+      row :lat
+      row :lon
+      row :address
+      row :map do |e|
+        text_node google_map(e.lat, e.lon)
+      end
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  
+  def google_map(lat, lon, zoom=18)
+    text = %{
+      <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+      <style type="text/css">#gmap_canvas img{max-width:none!important;background:none!important}</style>
+
+      <div style="overflow:hidden;height:500px;width:600px;">
+        <div id="gmap_canvas" style="height:500px;width:600px;"></div><a class="google-map-code" href="http://www.map-embed.com" id="get-map-data" name="get-map-data">map-embed.com</a>
+      </div>
+      
+      <script type="text/javascript">function init_map(){var myOptions = {zoom:#{zoom},center:new google.maps.LatLng(#{lat},#{lon}),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(#{lat}, #{lon})});}google.maps.event.addDomListener(window, 'load', init_map);</script>
+    }
+    
+    Rails.logger.info text
+    
+    text.html_safe
+  end
+  
+  
 end
