5e6114aeb4d4505ae91717aff6b72ff67d4cdba5
Ricky Chilcott <ricky@rbsavvy.com>
2016-04-21 22:11:03 -0400

    Styling

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "OBW Admin UI"
+  config.site_title = "OBWapp Admin"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -144,7 +144,7 @@ ActiveAdmin.setup do |config|
       username == ENV["ADMIN_USERNAME"] && password == ENV["ADMIN_PASSWORD"]
     end
   end
-  
+
   # == Setting a Favicon
   #
   # config.favicon = 'favicon.ico'
