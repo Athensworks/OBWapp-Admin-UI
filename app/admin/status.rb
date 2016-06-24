abd4163876e1e69a040fcf7e2874ac8af554b951
Ricky Chilcott <ricky@rbsavvy.com>
2016-06-23 22:22:54 -0400

    Remove report_countdown from statuses

--- app/admin/status.rb
+++ app/admin/status.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Status do
   permit_params :establishment_id, :beer_id, :status
-  
+
   actions :all, except: [:destroy, :new, :create]
 
   index do
@@ -12,10 +12,9 @@ ActiveAdmin.register Status do
     end
     column :last_out_update, sortable: true
     column :reported_out_count, sortable: true
-    column :report_countdown, sortable: true
     actions
   end
-  
+
   form do |f|
     inputs 'Details' do
       input :establishment, :input_html => { :disabled => true }
@@ -23,27 +22,25 @@ ActiveAdmin.register Status do
       input :status, as: :radio, collection: Status.status_options
       input :last_out_update, as: :string, :input_html => { :disabled => true }
       input :reported_out_count, :input_html => { :disabled => true }
-      input :report_countdown, :input_html => { :disabled => true }
     end
 
     actions
   end
-  
+
   scope :unknown
   scope :untapped
   scope :tapped
   scope :empty_reported
   scope :empty
   scope :cancelled
-  
-    
+
+
   controller do
     before_save do |status|
       status.last_out_update    = Time.now
       status.reported_out_count = 0
-      status.report_countdown   = 0
-      
-      ReportState.where(establishment_id: status.establishment_id, beer_id: status.beer_id).update_all(report_count: 0)
+
+      ReportState.where(establishment_id: status.establishment_id, beer_id: status.beer_id).delete_all
     end
   end
 end
